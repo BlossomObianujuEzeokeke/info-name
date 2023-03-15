c61211cc3cb46a3415112214c54cb47c78a892be
Blossom Obianuju Ezeokeke <ezeokekeobianuju@gmail.com>
2023-03-15 14:36:14 +0100

    react

--- src/component/Card.js
+++ src/component/Card.js
@@ -7,7 +7,7 @@ const Cards = () => {
 <div className="card--stats">
 <img src="../Images/Star 1.png" alt="Star" className="card--star"/>
 <span>5.0</span>
-<span className="gray">USA</span>
+<span className="gray--1">USA </span> 
 <span className="gray">(6) .</span>
 <p>Life lessons with Katie Zaferes</p>
 <p><b>From $136</b> / person</p>

31e31ca678470ab69ff2721fed025ed7277bd205
Blossom Obianuju Ezeokeke <ezeokekeobianuju@gmail.com>
2023-03-22 23:17:41 +0100

    State and hooks

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,19 +1,10 @@
 import React from 'react'
 
-const Card = (props) => {
+const   = () => {
   return (
-    <div className="card">
-<div className="card--badge">SOLD OUT</div>
-<img src={'../Images/&{props.img}'} className="card--image" />
-<div className="card--stats">
-<img src="../Images/Star 1 (1).png" className='card--star' />
-<span>{props.rating}</span>
-<span></span>
-
-
-</div>
-    </div>
+    <div> </div>
   )
 }
 
-export default Card
+export default  
+  
